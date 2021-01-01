9789420d02ef55af0c6b9fc5b803e2c86e91e34c
Walid <walid@air-de-walid.home>
2021-01-01 16:17:17 +0100

    restructuration

---
+++ helpers.py
@@ -0,0 +1,40 @@
+# Author: walid.daboubi@gmail.com
+# About: Predict big events using Hedonometer.org data
+# Version:
+    # v1 2020/12/31
+    # v1.1 2021/01/01
+    
+import argparse
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--train_from', help = 'Training data from date in "yyyy/mm/dd" format', required = True)
+    parser.add_argument('-u', '--train_until', help = 'Training data until date in "yyyy/mm/dd" format', required = True)
+    parser.add_argument('-d', '--future_depth', help = 'Number of days to predict', required = False)
+    parser.add_argument('-s', '--steps', help = 'Number of steps', required = False)
+    parser.add_argument('-l', '--language', help = 'Language. Available languages are ar_all, de_all, en_all, es_all, fr_all, id_all, ko_all, pt_all, ru_all ', required = False)
+    return vars(parser.parse_args())
+
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+    if len(sequence) == n_steps:
+        return sequence,None
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # find the end of this pattern
+        end_ix = i + n_steps
+        # check if we are beyond the sequence
+        if end_ix > len(sequence)-1:
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+def decompose_date(str_date):
+    return [int(x) for x in str_date.split('/')]
--- predict_hedonometer.py
+++ predict_hedonometer.py
@@ -1,119 +1,113 @@
-# walid.daboubi[a]gmail.com
-# Date:2020/12/31
+# Author: walid.daboubi@gmail.com
+# About: Predict big events using Hedonometer.org data
+# Version:
+    # v1 2020/12/31
+    # v1.1 2021/01/01
 
-import requests
-import pandas as pd
-from numpy import array
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import SimpleRNN
-from keras.layers import Dense
-import numpy as np
-import matplotlib.pyplot as plt
+from helpers import *
 
 URI = "http://hedonometer.org/api/v1/happiness"
+EPSILON = 10**-5
 
-# split a univariate sequence into samples
-def split_sequence(sequence, n_steps):
-    if len(sequence) == n_steps:
-        return sequence,None
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        # check if we are beyond the sequence
-        if end_ix > len(sequence)-1:
-            break
-        # gather input and output parts of the pattern
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return array(X), array(y)
+ARGS = get_args()
 
-data = requests.get(URI,params={'format': 'json','timeseries__title':'en_all','date__gte':'2014-01-01','limit':'10000'}).json()['objects']
+future_depth = 10
+if ARGS['future_depth'] != None:future_depth = int(ARGS['future_depth'])
+# Use n_steps last days to predit the current day
+n_steps = 60
+if ARGS['steps'] != None:n_steps = int(ARGS['steps'])
+language = 'en_all'
+if ARGS['language'] != None:language = ARGS['language']
+n_features = 1
+error_sum = 0
+errors, predictions, predictions_dates = [], [], []
+prediction_count = 0
+display_error = False
 
-df = pd.DataFrame(data)
+train_from_year, train_from_month, train_from_day = decompose_date(ARGS['train_from'])
+train_until_year, train_until_month, train_until_day = decompose_date(ARGS['train_until'])
+from_date_fomatted = '{}-{}-{}'.format(train_from_year, train_from_month, train_from_day)
+
+# Get data
+data = requests.get(URI, params = {'format': 'json','timeseries__title':language,'date__gte':from_date_fomatted,'limit':'100000'}).json()
+
+df = pd.DataFrame(data['objects'])
 df['date'] = pd.to_datetime(df.date)
 df.sort_values('date', inplace=True)
 df["happiness"] = df["happiness"].astype(float)
 
-split_date = pd.datetime(2021,1,1)
-
-# Testing data
-validation_data = df.loc[df['date'] >= split_date]
-validation_scores = np.array(validation_data["happiness"])
-validation_dates = np.array(validation_data["date"])
+train_until_date = pd.datetime(train_until_year, train_until_month, train_until_day)
+prediction_date = train_until_date + pd.to_timedelta(1, unit = 'd')
 
 # Training data
-training_data = df.loc[df['date'] < split_date]
+training_data = df.loc[df['date'] <= train_until_date]
 training_scores = np.array(training_data["happiness"])
 training_dates = np.array(training_data["date"])
 
-n_steps = 60
-future_depth = 60
-n_features = 1
-error_sum = 0
+# Testing data
+validation_data = df.loc[df['date'] > train_until_date]
+validation_scores = np.array(validation_data["happiness"])
+validation_dates = np.array(validation_data["date"])
 
-errors = []
-predictions = []
-predictions_dates = []
 
-prediction_count = 0
-new_date = split_date
-
+# Train and Predict
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import SimpleRNN
+from keras.layers import Dense
 while prediction_count < future_depth:
-    print ("*"*100+" prediction_count ="+str(prediction_count))
+    print ('-'*50+' prediction_count = {}'.format(prediction_count))
     size = training_scores.shape[0]
     # Train model with training data
     X, y = split_sequence(training_scores, n_steps)
     X = X.reshape((X.shape[0], X.shape[1], n_features))
-    # define model
+    # Define NN
     model = Sequential()
-    model.add(LSTM(20, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(LSTM(20, activation = 'relu', input_shape = (n_steps, n_features)))
     model.add(Dense(1))
-    model.compile(optimizer='adam', loss='mse')
-    # fit model
+    model.compile(optimizer = 'adam', loss='mse')
+    # Fit model
     model.fit(X, y, epochs=200, verbose=0)
     # Get last n_steps point as the features of the new next point to predict
     next_point_steps = training_scores[size-n_steps:]
     x_input, none = split_sequence(next_point_steps, n_steps)
     x_input = x_input.reshape((1, n_steps, n_features))
     yhat = model.predict(x_input, verbose = 0)[0][0]
+    print('Date: {}'.format(prediction_date))
+    print('Prediction: {}'.format(yhat))
     if prediction_count<len(validation_scores):#.indexOf(prediction_count)>-1:
+        display_error = True
         error = abs(validation_scores[prediction_count] - yhat)
         errors.append(error)
         error_sum += error
-        print("PREDICTION:"+str(yhat))
-        print("CURRENT ERROR:"+str(error))
-        print("MEAN ERROR:"+str(error_sum/(prediction_count+0.0000000001)))
+        print('Current Error: {}'.format(error))
+        print('Mean Error: {}'.format(error_sum/(prediction_count+EPSILON)))
         # Add the new prediction to training date
         predictions_dates.append(validation_dates[prediction_count])
         training_dates = np.append(training_dates,validation_dates[prediction_count])
-        print(type(validation_dates[prediction_count]))
     else:
         errors.append(0)
-        predictions_dates.append(np.datetime64(new_date))
-        training_dates = np.append(training_dates,np.datetime64(new_date))
-        new_date += pd.to_timedelta(1,unit='d')
+        predictions_dates.append(np.datetime64(prediction_date))
+        training_dates = np.append(training_dates,np.datetime64(prediction_date))
+        prediction_date += pd.to_timedelta(1,unit='d')
 
     predictions.append(yhat)
     training_scores = np.append(training_scores,yhat)
-    #training_dates = np.append(training_dates,validation_dates[prediction_count])
     prediction_count += 1
 
+# Convert arrays to np.array
 errors = np.array(errors)
 predictions = np.array(predictions)
 predictions_dates = np.array(predictions_dates)
-print(training_dates)
+
 # The division by 10 goal is to be able to see error in the same chart
-validation_scores = validation_scores/10
-training_scores = training_scores/10
-predictions = predictions/10
+validation_scores, training_scores, predictions = validation_scores/10, training_scores/10, predictions/10
+
+# Plot
 plt.plot(validation_dates, validation_scores, 'b', label = 'Actual')
 plt.plot(training_dates, training_scores, 'g', label = 'Training')
 plt.plot(predictions_dates, predictions, 'y', label = 'Prediction')
-
-plt.plot(predictions_dates, errors, 'r', label = 'Error')
-
+if display_error == True:
+    plt.plot(predictions_dates, errors, 'r', label = 'Error')
 plt.legend(loc = "best")
 plt.show()
